c440b66a6871d4b36883824f577c943c07b3c208
Sunbro-S <maksim.shukshin@urfu.me>
2024-04-25 20:17:28 +0500

    Minor manipulations

--- Identity.WebApi/Services/AuthService.cs
+++ Identity.WebApi/Services/AuthService.cs
@@ -41,18 +41,6 @@ namespace Identity.WebApi.Services
             return result.Succeeded;
         }
 
-        public async Task<bool> RegisterUser(LoginUser user)
-        {
-            var identityUser = new IdentityUser
-            {
-                UserName = user.UserName,
-                Email = user.UserName
-            };
-
-            var result = await _userManager.CreateAsync(identityUser, user.Password);
-            return result.Succeeded;
-        }
-
         public async Task<bool> Login(LoginUser user)
         {
             var identityUser = await _userManager.FindByEmailAsync(user.UserName);
